3027df7fcad8453871a79b4423a8d8e72da9b311
Pedro Henrique Suruagy Perrusi <suruagyperrusi@etu.unistra.fr>
2021-02-22 09:43:58 +0100

    Updated install path and removed unused config.h

--- src/ROS2Plugin/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sofa/config.h>
-
-#ifdef SOFA_BUILD_ROS2Plugin
-#  define ROS2Plugin_API SOFA_EXPORT_DYNAMIC_LIBRARY
-#else
-#  define ROS2Plugin_API SOFA_IMPORT_DYNAMIC_LIBRARY
-#endif
