fa24f24fadabaafe947491621508b72e4d16543a
Pedro Perrusi <suruagyperrusi@etu.unistra.fr>
2021-03-04 13:54:39 +0100

    ROS2Subscribers now use template specialization instead of inheritance

---
+++ src/ROS2Plugin/Subscribers/ROS2Subscriber.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <ROS2Plugin/ROS2Context.h>
+#include <ROS2Plugin/Subscribers/ROS2SubscriberNode.h>
+#include <ROS2Plugin/toolbox.h>
+#include <ROS2Plugin/types.h>
+#include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/core/objectmodel/DataCallback.h>
+#include <sofa/core/visual/VisualParams.h>
+#include <sofa/simulation/AnimateBeginEvent.h>
+
+namespace sofa {
+namespace ros2 {
+
+/**
+ * Base class for ROS2Plugin subscribers.
+ * When inheriting from this component, the Subscriber Node insertion into the ROS2 context is managed automatically.
+ * Only non-templated child classes have been tested for the moment.
+ *
+ * TODO: an automatic SOFA type deduction would benefit scene readability and prevent user-level bugs.
+ *
+ * Troubleshoot:
+ *      - toSofa() message conversions should be implemented for any new message
+ *      - draw() needs to be defined for the corresponding SofaType
+ *
+ * @tparam DataTypes SOFA data type should be informed by the child class implementation
+ * @tparam ROS2_MSG ROS message type should be informed by the child class implementation
+ */
+template <class DataTypes, class ROS2_MSG>
+class ROS2Subscriber : public core::objectmodel::BaseObject {
+   public:
+    SOFA_CLASS(SOFA_TEMPLATE2(ROS2Subscriber, DataTypes, ROS2_MSG), core::objectmodel::BaseObject);
+
+    core::objectmodel::SingleLink<ROS2Subscriber<DataTypes, ROS2_MSG>, ROS2Context, BaseLink::FLAG_STRONGLINK | BaseLink::FLAG_STOREPATH> l_ros2Context;
+
+    sofa::Data<DataTypes> d_output;
+    sofa::Data<std::string> d_NodeName;
+    sofa::Data<std::string> d_TopicName;
+    Data<double> d_drawScale;
+    Data<bool> d_draw;
+
+    std::shared_ptr<ROS2SubscriberNode<ROS2_MSG>> m_ros2node;
+
+    explicit ROS2Subscriber();
+    virtual ~ROS2Subscriber() = default;
+
+    virtual void init() override;
+    virtual void handleEvent(sofa::core::objectmodel::Event *event) override;
+    virtual void draw(const sofa::core::visual::VisualParams *vparams) override;
+
+    [[nodiscard]] std::string getTemplateName() const override { return templateName(this); }
+
+    static std::string templateName(const ROS2Subscriber<DataTypes, ROS2_MSG> * = nullptr) { return "Unknown"; }
+};
+
+}  // namespace ros2
+}  // end namespace sofa
---
+++ src/ROS2Plugin/Subscribers/ROS2Subscriber.inl
@@ -0,0 +1,45 @@
+#pragma once
+#include <ROS2Plugin/Subscribers/ROS2Subscriber.h>
+
+namespace sofa {
+namespace ros2 {
+
+template <class DataTypes, class ROS2_MSG>
+ROS2Subscriber<DataTypes, ROS2_MSG>::ROS2Subscriber()
+    : l_ros2Context(initLink("ros2Context", "ROS2 context link"))
+    , d_output(initData(&d_output, DataTypes(), "output", "output"))
+    , d_NodeName(initData(&d_NodeName, std::string("DefaultNodeName"), "nodeName", "Name of the ROS2 node"))
+    , d_TopicName(initData(&d_TopicName, std::string("DefaultTopicName"), "topicName", "Name of the ROS2 topic in which to publish"))
+    , d_drawScale(initData(&d_drawScale, 0.1, "drawScale", "Scale imposed to draw function in SOFA"))
+    , d_draw(initData(&d_draw, false, "draw", "If true, position is drawn on screen"))
+{
+    this->f_listening.setValue(true);
+}
+
+template <class DataTypes, class ROS2_MSG>
+void ROS2Subscriber<DataTypes, ROS2_MSG>::init()
+{
+    m_ros2node = std::make_shared<ROS2SubscriberNode<ROS2_MSG>>(d_NodeName.getValue(), d_TopicName.getValue());
+    l_ros2Context->addNode(m_ros2node);
+}
+
+template <class DataTypes, class ROS2_MSG>
+void ROS2Subscriber<DataTypes, ROS2_MSG>::handleEvent(sofa::core::objectmodel::Event *event)
+{
+    if (dynamic_cast<sofa::simulation::AnimateBeginEvent *>(event)) {
+        auto msg = m_ros2node->get();
+        d_output.setValue(toolbox::toSofa(msg));
+    }
+}
+
+template <class DataTypes, class ROS2_MSG>
+void ROS2Subscriber<DataTypes, ROS2_MSG>::draw(const sofa::core::visual::VisualParams *vparams)
+{
+    if (d_draw.getValue()) {
+        const auto &output = d_output.getValue();
+        toolbox::draw(vparams, output, d_drawScale.getValue());
+    }
+}
+
+}  // namespace ros2
+}  // end namespace sofa
