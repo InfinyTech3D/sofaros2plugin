412e95ca261b337df2f954b153aad9db28e484ff
pedroperrusi <phsuruagyperrusi@unistra.fr>
2021-03-19 14:24:41 +0100

    Fix: returns default message when topic does not exist

--- src/ros2plugin/subscribers/ROS2SubscriberNode.h
+++ src/ros2plugin/subscribers/ROS2SubscriberNode.h
@@ -15,19 +15,20 @@ template <class ROS2_MSG>
 class ROS2SubscriberNode : public rclcpp::Node {
    public:
     explicit ROS2SubscriberNode(const std::string &node_name = "DefaultNodeName", std::string topic_name = "DefaultTopicName", size_t buffer_size = 1)
-        : Node(node_name) {
+        : Node(node_name)
+    {
         m_subscription =
             this->create_subscription<ROS2_MSG>(topic_name, buffer_size, std::bind(&ROS2SubscriberNode::callback, this, std::placeholders::_1));
     }
 
-    ROS2_MSG get() const { return ROS2_MSG(*m_msg_ptr); /* could this copy be avoided ? */ }
+    ROS2_MSG get() const { return m_msg_ptr ? ROS2_MSG(*m_msg_ptr) : ROS2_MSG(); /* could this copy be avoided ? */ }
 
    protected:
     virtual void callback(const typename ROS2_MSG::SharedPtr msg) { m_msg_ptr = msg; }
 
    private:
     typename rclcpp::Subscription<ROS2_MSG>::SharedPtr m_subscription;
-    typename ROS2_MSG::SharedPtr                       m_msg_ptr;
+    typename ROS2_MSG::SharedPtr m_msg_ptr;
 };
 }  // namespace ros2
 }  // namespace sofa
