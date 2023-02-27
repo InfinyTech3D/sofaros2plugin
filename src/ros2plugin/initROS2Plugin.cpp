3356b92d7685e96936bd8ef7d750440bdb145afe
epernod <erik.pernod@gmail.com>
2023-02-27 23:26:05 +0100

    Fix windows dll import

--- src/ros2plugin/initROS2Plugin.cpp
+++ src/ros2plugin/initROS2Plugin.cpp
@@ -1,37 +1,58 @@
+
 #include <sofa/core/ObjectFactory.h>
 
 #include <string>
+
+#define ROS2PLUGIN_DYNAMIC_LIBRARY __declspec(dllexport)
 
 namespace sofa
 {
 namespace component
 {
-extern "C"
+
+extern "C" {
+    ROS2PLUGIN_DYNAMIC_LIBRARY void initExternalModule();
+    ROS2PLUGIN_DYNAMIC_LIBRARY const char* getModuleName();
+    ROS2PLUGIN_DYNAMIC_LIBRARY const char* getModuleVersion();
+    ROS2PLUGIN_DYNAMIC_LIBRARY const char* getModuleLicense();
+    ROS2PLUGIN_DYNAMIC_LIBRARY const char* getModuleDescription();
+    ROS2PLUGIN_DYNAMIC_LIBRARY const char* getModuleComponentList();
+}
+
+void initExternalModule()
 {
-    void initExternalModule()
+    static bool first = true;
+    if (first)
     {
-        static bool first = true;
-        if (first)
-        {
-            first = false;
-        }
+        first = false;
     }
+}
 
-    const char* getModuleName() { return "ROS2Plugin"; }
+const char* getModuleName()
+{
+    return "ROS2Plugin";
+}
 
-    const char* getModuleVersion() { return "0.0"; }
+const char* getModuleVersion()
+{
+    return "0.0";
+}
 
-    const char* getModuleLicense() { return "Private"; }
+const char* getModuleLicense()
+{
+    return "Private";
+}
 
-    const char* getModuleDescription() { return "ROS2 and SOFA interfaces"; }
 
-    const char* getModuleComponentList()
-    {
-        static std::string classes = sofa::core::ObjectFactory::getInstance()->listClassesFromTarget(sofa_tostring(SOFA_TARGET));
-        return classes.c_str();
-    }
+const char* getModuleDescription()
+{
+    return "ROS2 and SOFA interfaces";
+}
 
-}  // extern "C"
+const char* getModuleComponentList()
+{
+    return "";
+}
 
 }  // namespace component
 }  // namespace sofa
